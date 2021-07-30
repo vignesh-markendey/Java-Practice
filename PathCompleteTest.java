c4193e3741a6294aed2f2cd00d3dee78ce4dd0ad
vignesh-markendey <vig.markendey@gmail.com>
2021-07-30 15:08:06 +1000

    Committed two files: PathComplete.java and PathCompleteTest.java

---
+++ PathCompleteTest.java
@@ -0,0 +1,21 @@
+package HomePractice;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PathCompleteTest {
+    
+
+    @Test
+    public void testPath(){
+
+
+        Assert.assertEquals(10, PathComplete.findSomething(10, 8, 7));
+        Assert.assertEquals(10, PathComplete.findSomething(9, 8, 10));
+        Assert.assertEquals(10, PathComplete.findSomething(9, 10, 8));
+        Assert.assertEquals(10, PathComplete.findSomething(8, 9, 10));
+
+
+
+    }
+}
