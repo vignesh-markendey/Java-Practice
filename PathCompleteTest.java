75c29fd1bc907d5df4d86f1d428a934f81006860
vignesh-markendey <vig.markendey@gmail.com>
2021-07-30 15:26:41 +1000

    added comments regarding path info

--- PathCompleteTest.java
+++ PathCompleteTest.java
@@ -9,10 +9,16 @@ public class PathCompleteTest {
     @Test
     public void testPath(){
 
-
+        //checking path-1 (a > b > c)
         Assert.assertEquals(10, PathComplete.findSomething(10, 8, 7));
+
+        //checking path-2 (c > a > b)
         Assert.assertEquals(10, PathComplete.findSomething(9, 8, 10));
+
+        //checking path-3 (a < b > c)
         Assert.assertEquals(10, PathComplete.findSomething(9, 10, 8));
+
+        //checking path-4 (a < b < c)
         Assert.assertEquals(10, PathComplete.findSomething(8, 9, 10));
 
 
